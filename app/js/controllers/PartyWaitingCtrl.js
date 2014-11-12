55a7034474f0711aaaaa699b11763dc6739ba5cf
Bill Clinton <francis.duffeal.external@watchever.com>
2014-11-12 18:53:42 +0100

    refresh automatique

--- app/js/controllers/PartyWaitingCtrl.js
+++ app/js/controllers/PartyWaitingCtrl.js
@@ -1,48 +1,51 @@
 angular.module('myApp.controllers').controller('PartyWaitingCtrl',
-    ['$scope','rdv','$routeParams','user','$location','$filter','$rootScope',
-        function ($scope,rdv,$routeParams,user,$location,$filter,$rootScope) {
+    ['$scope','rdv','$routeParams','user','$location','$filter','$rootScope','$interval',
+        function ($scope,rdv,$routeParams,user,$location,$filter,$rootScope,$interval) {
             'use strict';
 
 			$scope.currentUser = user.get();
 
 
-
-	        rdv.get($routeParams.partyId).success(function(data){
-		        $scope.rdv = data;
-
-				$scope.isLeader = false;
-				$scope.canJoin = true;
-				$scope.imOnGroup = false;
-
-				$scope.profils = $filter('filterGameProfil')($scope.currentUser.userGame,$scope.rdv.game.id,$scope.rdv.plateform.id);
-
-				if($scope.profils[0]){
-					$scope.profilSelected = $scope.profils[0];
-				}
-
-				if($scope.currentUser !== null){
-					if($scope.rdv.leader && $scope.rdv.leader.username === $scope.currentUser.username){
-						$scope.isLeader = true;
-					}
-
-					for(var key in $scope.rdv.users){
-						if($scope.rdv.users[key].user.username === $scope.currentUser.username){
-							$scope.canJoin = false;
-							$scope.imOnGroup = true;
-						}
-					}
-
-					for(var key2 in $scope.rdv.usersInQueue){
-						if($scope.rdv.usersInQueue[key2].user.username === $scope.currentUser.username){
-							$scope.canJoin = false;
-						}
-					}
-				} else {
-					$scope.canJoin = false;
-				}
+	        var refreshData = function() {
 
 
-	        });
+		        rdv.get($routeParams.partyId).success(function (data) {
+			        $scope.rdv = data;
+
+			        $scope.isLeader = false;
+			        $scope.canJoin = true;
+			        $scope.imOnGroup = false;
+
+			        $scope.profils = $filter('filterGameProfil')($scope.currentUser.userGame, $scope.rdv.game.id, $scope.rdv.plateform.id);
+
+			        if ($scope.profils[0]) {
+				        $scope.profilSelected = $scope.profils[0];
+			        }
+
+			        if ($scope.currentUser !== null) {
+				        if ($scope.rdv.leader && $scope.rdv.leader.username === $scope.currentUser.username) {
+					        $scope.isLeader = true;
+				        }
+
+				        for (var key in $scope.rdv.users) {
+					        if ($scope.rdv.users[key].user.username === $scope.currentUser.username) {
+						        $scope.canJoin = false;
+						        $scope.imOnGroup = true;
+					        }
+				        }
+
+				        for (var key2 in $scope.rdv.usersInQueue) {
+					        if ($scope.rdv.usersInQueue[key2].user.username === $scope.currentUser.username) {
+						        $scope.canJoin = false;
+					        }
+				        }
+			        } else {
+				        $scope.canJoin = false;
+			        }
+
+
+		        });
+	        };
 
 			$scope.join = function(){
 				if($scope.currentUser === null){
@@ -81,6 +84,11 @@ angular.module('myApp.controllers').controller('PartyWaitingCtrl',
 			};
 
 			$scope.currentUrl = $location.absUrl();
+
+	        refreshData();
+	        $interval(function(){
+				refreshData();
+	        }, 10000);
 
         }
     ]
