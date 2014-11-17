edc84042699fd131d7778e8425dbc9bdfd3d26d4
Bill Clinton <francis.duffeal.external@watchever.com>
2014-11-17 17:49:44 +0100

    ajout des filterWords et filterCharacters

---
+++ app/js/filters/filterWords.js
@@ -0,0 +1,18 @@
+angular.module('myApp.filters').filter('filterWords', function () {
+	'use strict';
+	return function (input, words) {
+		if (isNaN(words)) {
+			return input;
+		}
+		if (words <= 0) {
+			return '';
+		}
+		if (input) {
+			var inputWords = input.split(/\s+/);
+			if (inputWords.length > words) {
+				input = inputWords.slice(0, words).join(' ') + '...';
+			}
+		}
+		return input;
+	};
+});
