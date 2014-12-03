1aec3092f688786a77015216eda5e8a96cc454f8
Bill Clinton <francis.duffeal.external@watchever.com>
2014-12-03 17:53:25 +0100

    games-profiles

--- app/js/controllers/GamesProfilesCtrl.js
+++ app/js/controllers/GamesProfilesCtrl.js
@@ -1,9 +1,67 @@
 angular.module('myApp.controllers').controller('GamesProfilesCtrl',
-    ['$scope','$routeParams',
-        function ($scope,$routeParams) {
+    ['$scope','user','rdv','$routeParams','redirection','lang',
+        function ($scope,user,rdv,$routeParams,redirection,lang) {
             'use strict';
-            $scope.msg = $routeParams.msg;
-            console.log($scope.msg);
+	        $scope.lang = lang.getCurrent();
+
+	        $scope.currentUser = user.get();
+
+	        rdv.getFormInfo().then(function(data){
+		        $scope.plateforms = data.plateforms;
+		        $scope.games = data.games;
+
+		        $scope.plateform = data.plateforms[0];
+		        if($routeParams.plateformId){
+			        for(var key in data.plateforms){
+				        if(data.plateforms[key].id == $routeParams.plateformId){
+					        $scope.plateform = data.plateforms[key];
+				        }
+			        }
+		        }
+
+		        $scope.game = data.games[0];
+		        if($routeParams.gameId){
+			        for(var key in data.games){
+				        if(data.games[key].id == $routeParams.gameId){
+					        $scope.game = data.games[key];
+				        }
+			        }
+		        }
+
+		        $scope.updateFormData();
+
+	        });
+
+
+	        $scope.submitFormGameInfo = function(){
+		        if($scope.gameInfo.$valid === false){
+			        return;
+		        }
+		        $scope.formGameInfoUpdate = false;
+		        user.setUserGame($scope.plateform.id,$scope.game.id,$scope.profilName,$scope.gameUsername,$scope.data1,$scope.data2,$scope.data3,$scope.data4).success(function(data){
+			        $scope.formGameInfoUpdate = true;
+		        });
+	        };
+
+	        $scope.updateFormData = function(){
+		        $scope.formGameInfoUpdate = false;
+		        $scope.profilName = '';
+		        $scope.gameUsername = '';
+		        $scope.data1 = '';
+		        $scope.data2 = '';
+		        $scope.data3 = '';
+		        $scope.data4 = '';
+		        for(var key in $scope.currentUser.userGame){
+			        if($scope.currentUser.userGame[key].plateform.id === $scope.plateform.id){
+				        $scope.profilName = $scope.currentUser.userGame[key].gameProfilName;
+				        $scope.gameUsername = $scope.currentUser.userGame[key].gameUsername;
+				        $scope.data2 = $scope.currentUser.userGame[key].data2;
+				        $scope.data3 = $scope.currentUser.userGame[key].data3;
+				        $scope.data4 = $scope.currentUser.userGame[key].data4;
+				        $scope.data1 = $scope.currentUser.userGame[key].data1;
+			        }
+		        }
+	        };
         }
     ]
 );
