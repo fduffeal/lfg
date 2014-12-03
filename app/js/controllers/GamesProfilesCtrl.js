e545715afe54b363e7807c4f15ccdefe887f15d3
Fumistar <francis.duffeal@gmail.com>
2014-12-03 21:27:39 +0100

    update create profil game

--- app/js/controllers/GamesProfilesCtrl.js
+++ app/js/controllers/GamesProfilesCtrl.js
@@ -5,46 +5,83 @@ angular.module('myApp.controllers').controller('GamesProfilesCtrl',
 	        $scope.lang = lang.getCurrent();
 
 	        $scope.currentUser = user.get();
+			$scope.aProfils = [];
+			if($scope.currentUser !== null){
+				$scope.aProfils = $scope.currentUser.userGame;
+			}
+
 
 	        rdv.getFormInfo().then(function(data){
 		        $scope.plateforms = data.plateforms;
 		        $scope.games = data.games;
 
-		        $scope.plateform = data.plateforms[0];
-		        if($routeParams.plateformId){
+		        //$scope.plateform = data.plateforms[0];
+		        /*if($routeParams.plateformId){
 			        for(var key in data.plateforms){
 				        if(data.plateforms[key].id == $routeParams.plateformId){
 					        $scope.plateform = data.plateforms[key];
 				        }
 			        }
-		        }
+		        }*/
 
-		        $scope.game = data.games[0];
-		        if($routeParams.gameId){
+		        //$scope.game = data.games[0];
+		        /*if($routeParams.gameId){
 			        for(var key in data.games){
 				        if(data.games[key].id == $routeParams.gameId){
 					        $scope.game = data.games[key];
 				        }
 			        }
-		        }
+		        }*/
 
-		        $scope.updateFormData();
+		        //$scope.updateFormData();
 
 	        });
 
 
 	        $scope.submitFormGameInfo = function(){
-		        if($scope.gameInfo.$valid === false){
+		        if($scope.formGameProfil.$valid === false){
 			        return;
 		        }
 		        $scope.formGameInfoUpdate = false;
-		        user.setUserGame($scope.plateform.id,$scope.game.id,$scope.profilName,$scope.gameUsername,$scope.data1,$scope.data2,$scope.data3,$scope.data4).success(function(data){
-			        $scope.formGameInfoUpdate = true;
-		        });
+
+				if(typeof $scope.profilId  === "undefined" || $scope.profilId  === ""){
+					user.createUserGame($scope.plateform.id,$scope.game.id,$scope.profilName,$scope.gameUsername,$scope.data1,$scope.data2,$scope.data3,$scope.data4).success(function(data){
+						$scope.formGameInfoUpdate = true;
+					});
+				}else {
+					user.updateUserGame($scope.plateform.id,$scope.game.id,$scope.profilId,$scope.profilName,$scope.gameUsername,$scope.data1,$scope.data2,$scope.data3,$scope.data4).success(function(data){
+						$scope.formGameInfoUpdate = true;
+					});
+				}
+
 	        };
 
+			$scope.$watch('plateform',function(old,nexV){
+				console.log('plateform',old,nexV);
+			});
+
+			$scope.selectProfil = function(profil){
+
+				$scope.profilId = (profil!==null) ? profil.id : '';
+				$scope.profilName = (profil!==null) ? profil.gameProfilName : '';
+				$scope.gameUsername = (profil!==null) ? profil.gameUsername : '';
+				$scope.data1 = (profil!==null) ? profil.data1 : '';
+				$scope.data2 = (profil!==null) ? profil.data2 : '';
+				$scope.data3 = (profil!==null) ? profil.data3 : '';
+				$scope.data4 = (profil!==null) ? profil.data4 : '';
+
+				$scope.plateform = (profil!==null) ? profil.plateform : '';
+				$scope.game = (profil!==null) ? profil.game : '';
+
+			};
+
 	        $scope.updateFormData = function(){
-		        $scope.formGameInfoUpdate = false;
+
+				if($scope.formGameProfil.$valid === false){
+					return;
+				}
+
+		        /*$scope.formGameInfoUpdate = false;
 		        $scope.profilName = '';
 		        $scope.gameUsername = '';
 		        $scope.data1 = '';
@@ -60,7 +97,7 @@ angular.module('myApp.controllers').controller('GamesProfilesCtrl',
 				        $scope.data4 = $scope.currentUser.userGame[key].data4;
 				        $scope.data1 = $scope.currentUser.userGame[key].data1;
 			        }
-		        }
+		        }*/
 	        };
         }
     ]
