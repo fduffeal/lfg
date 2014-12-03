63ffe9b87cab379ae9d6025493d6706267dc1c7e
Bill Clinton <francis.duffeal.external@watchever.com>
2014-12-03 16:37:48 +0100

    notifications

--- app/js/controllers/NotificationCtrl.js
+++ app/js/controllers/NotificationCtrl.js
@@ -2,8 +2,36 @@ angular.module('myApp.controllers').controller('NotificationCtrl',
     ['$scope','$routeParams',
         function ($scope,$routeParams) {
             'use strict';
-            $scope.msg = $routeParams.msg;
-            console.log($scope.msg);
+
+	        $scope.partyWaitingUrlRoot = redirection.getPartyWaitingUrlRoot();
+
+	        /**
+	         * autoRefreshDataNotif
+	         */
+	        var refreshDataNotif = function(){
+		        rdv.getNotifications().success(function(data){
+			        if($scope.userInfo === null){
+				        return;
+			        }
+			        $scope.notifications = $filter('filterNotification')(data,$scope.userInfo.id);
+		        });
+	        };
+
+	        var refreshTime = 12000;
+	        var autoRefreshDataNotif = function(){
+		        $interval.cancel($scope.intervaNotificationId);
+
+		        if (angular.isDefined($scope.intervaNotificationId)) {
+			        return;
+		        }
+
+		        $scope.intervaNotificationId = $interval(function(){
+			        refreshDataNotif();
+		        }, refreshTime);
+	        };
+
+	        refreshDataNotif();
+	        autoRefreshDataNotif();
         }
     ]
 );
