28c59d3f7ef02620a67f83a1d79205dde08c0f6a
Fumistar <francis.duffeal@gmail.com>
2014-12-31 15:33:35 +0100

    bower component

--- chat/socket.js
+++ chat/socket.js
@@ -1,6 +1,6 @@
 // Setup basic express server
 var express = require('express');
-var app = express()
+var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
---
+++ chat/socket_close.js
@@ -0,0 +1,8 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+io.server.close();
