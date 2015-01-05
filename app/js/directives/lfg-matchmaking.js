cf5aaee86895042721ddfcb14dc965c8dbc15de9
Bill Clinton <francis.duffeal.external@watchever.com>
2015-01-05 17:29:28 +0100

    clean console.log

--- app/js/directives/lfg-matchmaking.js
+++ app/js/directives/lfg-matchmaking.js
@@ -39,7 +39,6 @@ angular.module('myApp.directives')
 	                };
 
 	                $scope.$watch('templateSelectedModel',function(newValue,oldValue){
-		               console.log('templateSelectedModel',newValue);
 		                $scope.$emit('templateSelectedModel',[newValue]);
 	                });
 
