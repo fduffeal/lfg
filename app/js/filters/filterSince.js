1d8532c346a8f22562701b40fadf7c14f49cdcf8
Bill Clinton <francis.duffeal.external@watchever.com>
2015-01-12 17:36:29 +0100

    since + home message

--- app/js/filters/filterSince.js
+++ app/js/filters/filterSince.js
@@ -6,23 +6,32 @@ angular.module('myApp.filters').filter('filterSince', function () {
 		// Calculate the difference in milliseconds
 		var difference_ms = date1_ms - (date2_ms*1000);
 
+		if(difference_ms < 0){
+			now.setTime(date2_ms*1000);
+			var month = now.getMonth()+1;
+			if(month < 10){
+				month = "0"+month;
+			}
+			return now.getFullYear()+"-"+month+"-"+now.getDate()+" "+now.getHours()+":"+now.getMinutes();
+		}
+
 		var one_minute=1000*60*1;
 
 		var diffMinutes = Math.round(difference_ms/one_minute);
 		if(diffMinutes < 60){
-			return diffMinutes+' minutes';
+			return diffMinutes+' minutes ago';
 		}
 
 		var one_hour=1000*60*60*1;
 		var diffHours = Math.round(difference_ms/one_hour);
 		if(diffHours < 24){
-			return diffHours+' hours';
+			return diffHours+' hours ago';
 		}
 
 		//Get 1 day in milliseconds
 		var one_day=1000*60*60*24;
 		var diffDays = Math.round(difference_ms/one_day);
-		return diffDays+' days';
+		return diffDays+' days ago';
 
 	};
 });
