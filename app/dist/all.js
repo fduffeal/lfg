6a18728d6abd761882214b70cddabf499818ffee
Bill Clinton <francis.duffeal.external@watchever.com>
2015-02-10 15:23:58 +0100

    directive lfg-since autorefresh time

--- app/dist/all.js
+++ app/dist/all.js
@@ -31842,6 +31842,33 @@ angular.module('myApp.directives')
     ]
 );
 
+angular.module('myApp.directives')
+    .directive('lfgSince', ['tools','$timeout',
+        function(tools,$timeout) {
+            'use strict';
+            return {
+                scope:{
+                    time : '@'
+                },
+                link: function($scope, element, attrs) {
+
+                    var update = function(){
+                        $scope.message = tools.getSinceTimeMessage($scope.time);
+                        $timeout(function() {
+                            update();
+                        }, 60000);
+                    };
+
+                    update();
+
+                },
+                restrict: 'E',
+                templateUrl: '/html/directives/lfg-since.html'
+            };
+        }
+    ]
+);
+
 angular.module('myApp.filters').filter('filterAvatar', [function () {
 	'use strict';
 	return function (userGameProfil) {
@@ -32076,49 +32103,11 @@ angular.module('myApp.filters').filter('filterRdvWithMe', [
 
 	};
 }]);
-angular.module('myApp.filters').filter('filterSince', [
-	'$filter','gettextCatalog',
-	function ($filter,gettextCatalog) {
+angular.module('myApp.filters').filter('filterSince', ['tools',
+	function (tools) {
 		'use strict';
-		return function (date2_s) {
-			var date2_ms = date2_s*1000;
-			var currentDate = new Date(date2_ms);
-
-			var now = new Date();
-			var date1_ms = now.getTime();
-			// Calculate the difference in milliseconds
-			var difference_ms = date1_ms - date2_ms;
-
-			var tomorrow = new Date(date1_ms + 24 * 60 * 60 * 1000);
-
-			if(difference_ms < 0){
-
-				if(tomorrow.getDay() == currentDate.getDay()){
-					return gettextCatalog.getString("tomorrow at {{time}}", { time: $filter('date')(date2_ms, 'HH:mm') });
-				} else if (now.getDay() == currentDate.getDay()){
-					return gettextCatalog.getString("today at {{time}}", { time: $filter('date')(date2_ms, 'HH:mm') });
-				}
-				return $filter('date')(date2_ms, 'd/MM/yy HH:mm');
-			}
-
-			var one_minute=1000*60*1;
-
-			var diffMinutes = Math.round(difference_ms/one_minute);
-			if(diffMinutes < 60){
-				return gettextCatalog.getString("{{minutes}} minutes ago", { minutes: diffMinutes });
-			}
-
-			var one_hour=1000*60*60*1;
-			var diffHours = Math.round(difference_ms/one_hour);
-			if(diffHours < 24){
-				return gettextCatalog.getString("{{hours}} hours ago", { hours: diffHours });
-
-			}
-
-			//Get 1 day in milliseconds
-			var one_day=1000*60*60*24;
-			var diffDays = Math.round(difference_ms/one_day);
-			return gettextCatalog.getString("{{days}} days ago", { days: diffDays });
+		return function (time_s) {
+			return tools.getSinceTimeMessage(time_s);
 		};
 	}
 ]);
@@ -33152,6 +33141,55 @@ angular.module('myApp.services')
 		}
 	]
 );
+angular.module('myApp.services')
+	.service('tools', ['gettextCatalog','$filter',
+		function(gettextCatalog,$filter) {
+			'use strict';
+			this.getSinceTimeMessage = function(date2_s) {
+
+				var date2_ms = date2_s * 1000;
+				var currentDate = new Date(date2_ms);
+
+				var now = new Date();
+				var date1_ms = now.getTime();
+				// Calculate the difference in milliseconds
+				var difference_ms = date1_ms - date2_ms;
+
+				var tomorrow = new Date(date1_ms + 24 * 60 * 60 * 1000);
+
+				if (difference_ms < 0) {
+
+					if (tomorrow.getDay() == currentDate.getDay()) {
+						return gettextCatalog.getString("tomorrow at {{time}}", {time: $filter('date')(date2_ms, 'HH:mm')});
+					} else if (now.getDay() == currentDate.getDay()) {
+						return gettextCatalog.getString("today at {{time}}", {time: $filter('date')(date2_ms, 'HH:mm')});
+					}
+					return $filter('date')(date2_ms, 'd/MM/yy HH:mm');
+				}
+
+				var one_minute = 1000 * 60 * 1;
+
+				var diffMinutes = Math.round(difference_ms / one_minute);
+				if (diffMinutes < 60) {
+					return gettextCatalog.getString("{{minutes}} minutes ago", {minutes: diffMinutes});
+				}
+
+				var one_hour = 1000 * 60 * 60 * 1;
+				var diffHours = Math.round(difference_ms / one_hour);
+				if (diffHours < 24) {
+					return gettextCatalog.getString("{{hours}} hours ago", {hours: diffHours});
+
+				}
+
+				//Get 1 day in milliseconds
+				var one_day = 1000 * 60 * 60 * 24;
+				var diffDays = Math.round(difference_ms / one_day);
+				return gettextCatalog.getString("{{days}} days ago", {days: diffDays});
+			}
+		}
+	]
+);
+
 angular.module('myApp.services')
 	.service('user', ['$http','storage','api','$rootScope','$q','$timeout','$window','socket',
 		function($http,storage,api,$rootScope,$q,$timeout,$window,socket) {
