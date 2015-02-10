502f8d5431dbe98e937d6421967202be63b2e6de
Bill Clinton <francis.duffeal.external@watchever.com>
2015-02-10 11:39:13 +0100

    mise à jour des lib + nouvelle gestion de la page rdv (soft add)

--- app/bower_components/socket.io/test/socket.io.js
+++ app/bower_components/socket.io/test/socket.io.js
@@ -15,7 +15,7 @@ function client(srv, nsp, opts){
   }
   var addr = srv.address();
   if (!addr) addr = srv.listen().address();
-  var url = 'ws://' + addr.address + ':' + addr.port + (nsp || '');
+  var url = 'ws://localhost:' + addr.port + (nsp || '');
   return ioc(url, opts);
 }
 
@@ -127,7 +127,7 @@ describe('socket.io', function(){
         expect(s.handshake.time.split(' ').length > 0); // Is "multipart" string representation
 
         // Address, xdomain, secure, issued and url set
-        expect(s.handshake.address).to.be('127.0.0.1');
+        expect(s.handshake.address).to.contain('127.0.0.1');
         expect(s.handshake.xdomain).to.be.a('boolean');
         expect(s.handshake.secure).to.be.a('boolean');
         expect(s.handshake.issued).to.be.a('number');
@@ -302,6 +302,21 @@ describe('socket.io', function(){
           done();
         });
     });
+
+    it('should allow request when origin defined as function and no origin is supplied', function(done) {
+      var sockets = io({ origins: function(origin,callback){
+        if (origin == '*') {
+          return callback(null, true);
+        }
+        return callback(null, false);
+      } }).listen('54021');
+      request.get('http://localhost:54021/socket.io/default/')
+       .query({ transport: 'polling' })
+       .end(function (err, res) {
+          expect(res.status).to.be(200);
+          done();
+        });
+    });
   });
 
   describe('close', function(){
@@ -447,7 +462,7 @@ describe('socket.io', function(){
       var c1 = client(srv, '/');
       var c2 = client(srv, '/abc');
     });
-    
+
     it('should be equivalent for "" and "/" on client', function(done){
       var srv = http();
       var sio = io(srv);
@@ -456,7 +471,7 @@ describe('socket.io', function(){
       });
       var c1 = client(srv, '');
     });
-    
+
     it('should work with `of` and many sockets', function(done){
       var srv = http();
       var sio = io(srv);
@@ -800,6 +815,208 @@ describe('socket.io', function(){
       });
     });
 
+    it('should not emit volatile event after regular event (polling)', function(done) {
+      var srv = http();
+      var sio = io(srv, { transports: ['polling'] });
+
+      var counter = 0;
+      srv.listen(function(){
+        sio.on('connection', function(s){
+          s.emit('ev', 'data');
+          s.volatile.emit('ev', 'data');
+        });
+
+        var socket = client(srv, { transports: ['polling'] });
+        socket.on('ev', function() {
+          counter++;
+        });
+      });
+
+      setTimeout(function() {
+        expect(counter).to.be(1);
+        done();
+      }, 200);
+    });
+
+    it('should not emit volatile event after regular event (ws)', function(done) {
+      var srv = http();
+      var sio = io(srv, { transports: ['websocket'] });
+
+      var counter = 0;
+      srv.listen(function(){
+        sio.on('connection', function(s){
+          s.emit('ev', 'data');
+          s.volatile.emit('ev', 'data');
+        });
+
+        var socket = client(srv, { transports: ['websocket'] });
+        socket.on('ev', function() {
+          counter++;
+        });
+      });
+
+      setTimeout(function() {
+        expect(counter).to.be(1);
+        done();
+      }, 200);
+    });
+
+    it('should emit volatile event (polling)', function(done) {
+      var srv = http();
+      var sio = io(srv, { transports: ['polling'] });
+
+      var counter = 0;
+      srv.listen(function(){
+        sio.on('connection', function(s){
+          // Wait to make sure there are no packets being sent for opening the connection
+          setTimeout(function() {
+            s.volatile.emit('ev', 'data');
+          }, 20);
+        });
+
+        var socket = client(srv, { transports: ['polling'] });
+        socket.on('ev', function() {
+          counter++;
+        });
+      });
+
+      setTimeout(function() {
+        expect(counter).to.be(1);
+        done();
+      }, 200);
+    });
+
+    it('should emit volatile event (ws)', function(done) {
+      var srv = http();
+      var sio = io(srv, { transports: ['websocket'] });
+
+      var counter = 0;
+      srv.listen(function(){
+        sio.on('connection', function(s){
+          // Wait to make sure there are no packets being sent for opening the connection
+          setTimeout(function() {
+            s.volatile.emit('ev', 'data');
+          }, 20);
+        });
+
+        var socket = client(srv, { transports: ['websocket'] });
+        socket.on('ev', function() {
+          counter++;
+        });
+      });
+
+      setTimeout(function() {
+        expect(counter).to.be(1);
+        done();
+      }, 200);
+    });
+
+    it('should emit only one consecutive volatile event (polling)', function(done) {
+      var srv = http();
+      var sio = io(srv, { transports: ['polling'] });
+
+      var counter = 0;
+      srv.listen(function(){
+        sio.on('connection', function(s){
+          // Wait to make sure there are no packets being sent for opening the connection
+          setTimeout(function() {
+            s.volatile.emit('ev', 'data');
+            s.volatile.emit('ev', 'data');
+          }, 20);
+        });
+
+        var socket = client(srv, { transports: ['polling'] });
+        socket.on('ev', function() {
+          counter++;
+        });
+      });
+
+      setTimeout(function() {
+        expect(counter).to.be(1);
+        done();
+      }, 200);
+    });
+
+    it('should emit only one consecutive volatile event (ws)', function(done) {
+      var srv = http();
+      var sio = io(srv, { transports: ['websocket'] });
+
+      var counter = 0;
+      srv.listen(function(){
+        sio.on('connection', function(s){
+          // Wait to make sure there are no packets being sent for opening the connection
+          setTimeout(function() {
+            s.volatile.emit('ev', 'data');
+            s.volatile.emit('ev', 'data');
+          }, 20);
+        });
+
+        var socket = client(srv, { transports: ['websocket'] });
+        socket.on('ev', function() {
+          counter++;
+        });
+      });
+
+      setTimeout(function() {
+        expect(counter).to.be(1);
+        done();
+      }, 200);
+    });
+
+    it('should emit regular events after trying a failed volatile event (polling)', function(done) {
+      var srv = http();
+      var sio = io(srv, { transports: ['polling'] });
+
+      var counter = 0;
+      srv.listen(function(){
+        sio.on('connection', function(s){
+          // Wait to make sure there are no packets being sent for opening the connection
+          setTimeout(function() {
+            s.emit('ev', 'data');
+            s.volatile.emit('ev', 'data');
+            s.emit('ev', 'data');
+          }, 20);
+        });
+
+        var socket = client(srv, { transports: ['polling'] });
+        socket.on('ev', function() {
+          counter++;
+        });
+      });
+
+      setTimeout(function() {
+        expect(counter).to.be(2);
+        done();
+      }, 200);
+    });
+
+    it('should emit regular events after trying a failed volatile event (ws)', function(done) {
+      var srv = http();
+      var sio = io(srv, { transports: ['websocket'] });
+
+      var counter = 0;
+      srv.listen(function(){
+        sio.on('connection', function(s){
+          // Wait to make sure there are no packets being sent for opening the connection
+          setTimeout(function() {
+            s.emit('ev', 'data');
+            s.volatile.emit('ev', 'data');
+            s.emit('ev', 'data');
+          }, 20);
+        });
+
+        var socket = client(srv, { transports: ['websocket'] });
+        socket.on('ev', function() {
+          counter++;
+        });
+      });
+
+      setTimeout(function() {
+        expect(counter).to.be(2);
+        done();
+      }, 200);
+    });
+
     it('should emit message events through `send`', function(done){
       var srv = http();
       var sio = io(srv);
@@ -1022,7 +1239,7 @@ describe('socket.io', function(){
       var sio = io(srv);
       srv.listen(function() {
         var addr = srv.listen().address();
-        var url = 'ws://' + addr.address + ':' + addr.port + '?key1=1&key2=2';
+        var url = 'ws://localhost:' + addr.port + '?key1=1&key2=2';
         var socket = ioc(url);
         sio.on('connection', function(s) {
           var parsed = require('url').parse(s.request.url);
@@ -1035,7 +1252,7 @@ describe('socket.io', function(){
     });
 
     it('should handle very large json', function(done){
-      this.timeout();
+      this.timeout(30000);
       var srv = http();
       var sio = io(srv);
       var received = 0;
@@ -1082,6 +1299,66 @@ describe('socket.io', function(){
           s.on('big', function(a){
             expect(Buffer.isBuffer(a.image)).to.be(true);
             s.emit('big', a);
+          });
+        });
+      });
+    });
+
+    it('should be able to emit after server close and restart', function(done){
+      var srv = http();
+      var sio = io(srv);
+
+      sio.on('connection', function(socket){
+        socket.on('ev', function(data){
+          expect(data).to.be('payload');
+          done();
+        });
+      });
+
+      srv.listen(function(){
+        var port = srv.address().port;
+        var clientSocket = client(srv, { reconnectionAttempts: 10, reconnectionDelay: 100 });
+        clientSocket.once('connect', function(){
+          srv.close(function(){
+            srv.listen(port, function(){
+              clientSocket.on('reconnect', function(){
+                clientSocket.emit('ev', 'payload');
+              });
+            });
+          });
+        });
+      });
+    });
+
+    it('should error with raw binary and warn', function(done){
+      var srv = http();
+      var sio = io(srv);
+      srv.listen(function(){
+        var socket = client(srv);
+        sio.on('connection', function(s){
+          s.conn.on('upgrade', function(){
+            console.log('\033[96mNote: warning expected and normal in test.\033[39m');
+            socket.io.engine.write('5woooot');
+            setTimeout(function(){
+              done();
+            }, 100);
+          });
+        });
+      });
+    });
+
+    it('should not crash with raw binary', function(done){
+      var srv = http();
+      var sio = io(srv);
+      srv.listen(function(){
+        var socket = client(srv);
+        sio.on('connection', function(s){
+          s.once('error', function(err){
+            expect(err.message).to.match(/Illegal attachments/);
+            done();
+          });
+          s.conn.on('upgrade', function(){
+            socket.io.engine.write('5woooot');
           });
         });
       });
@@ -1385,6 +1662,30 @@ describe('socket.io', function(){
         });
       });
     });
+
+    it('should properly cleanup left rooms', function(done){
+      var srv = http();
+      var sio = io(srv);
+
+      srv.listen(function(){
+        var socket = client(srv);
+        sio.on('connection', function(s){
+          s.join('a', function(){
+            expect(s.rooms).to.eql([s.id, 'a']);
+            s.join('b', function(){
+              expect(s.rooms).to.eql([s.id, 'a', 'b']);
+              s.leave('unknown', function(){
+                expect(s.rooms).to.eql([s.id, 'a', 'b']);
+                s.leaveAll();
+                expect(s.rooms).to.eql([]);
+                done();
+              });
+            });
+          });
+        });
+      });
+    });
+
   });
 
   describe('middleware', function(done){
--- app/js/controllers/RdvCtrl.js
+++ app/js/controllers/RdvCtrl.js
@@ -79,12 +79,55 @@ angular.module('myApp.controllers').controller('RdvCtrl',
 				}
 			};
 
+			var arrayUnique = function(array) {
+				var a = array.concat();
+				for(var i=0; i<a.length; ++i) {
+					for(var j=i+1; j<a.length; ++j) {
+						if(a[i] === a[j])
+							a.splice(j--, 1);
+					}
+				}
+				return a;
+			};
+
+			var addNewData = function(array,newDataArray){
+				for(var j=0;j<newDataArray.length;j++){
+					var alreadyAdded = false;
+					for(var i=0;i< array.length;i++){
+
+						if(newDataArray[j].id === array[i].id && newDataArray[j].type === array[i].type){
+							alreadyAdded = true;
+						}
+					}
+
+					if(alreadyAdded === false){
+						array.push(newDataArray[j]);
+					}
+				}
+			};
+
+
+			var updateMyRdv = function(){
+				//filterRdvWithMe:currentUser.id:plateformSelected:tags:slotMinAvailable:slotMaxAvailable
+				if($scope.currentUser !== null){
+
+					if(typeof $scope.plateform === "undefined" || $scope.plateform  === null){
+						var plateformId = null;
+					}else {
+						var plateformId = $scope.plateform.id;
+					}
+
+					//if($scope.plateform)
+					$scope.aMyRdv = $filter('filterRdvWithMe')($scope.aRdv,$scope.currentUser.id,plateformId,$scope.tags,$scope.slotMinAvailable,$scope.slotMaxAvailable);
+				}
+			};
+
+
 
 			var refreshRdvData = function(){
 
 				annonce.get().success(function(data) {
-					formatAnnonces(data);
-					$scope.aRdv = $scope.aAnnoncesFormated.concat($scope.aRdvNormaux);
+					addNewData($scope.aRdv,formatAnnonces(data));
 				});
 
 				rdv.getAll().success(function(data) {
@@ -95,21 +138,9 @@ angular.module('myApp.controllers').controller('RdvCtrl',
 						formatRdv(data[key]);
 					}
 
-					$scope.aRdvNormaux = data;
-					$scope.aRdv = $scope.aRdvNormaux.concat($scope.aAnnoncesFormated);
+					addNewData($scope.aRdv,data);
+					updateMyRdv();
 
-					//filterRdvWithMe:currentUser.id:plateformSelected:tags:slotMinAvailable:slotMaxAvailable
-					if($scope.currentUser !== null){
-
-						if(typeof $scope.plateform === "undefined"){
-							var plateformId = null;
-						}else {
-							var plateformId = $scope.plateform.id;
-						}
-
-						//if($scope.plateform)
-						$scope.aMyRdv = $filter('filterRdvWithMe')(data,$scope.currentUser.id,plateformId,$scope.tags,$scope.slotMinAvailable,$scope.slotMaxAvailable);
-					}
 				}).error(function(data, status, headers, config) {
 					// called asynchronously if an error occurs
 					// or server returns response with an error status.
@@ -120,7 +151,7 @@ angular.module('myApp.controllers').controller('RdvCtrl',
 			$scope.displayWelcome = ($route.current.action === 'welcome');
 
 			//autoRefresh
-			var refreshTime = 12000;
+			var refreshTime = 20000;
 			var autoRefreshData = function(){
 				$scope.intervalId = $interval(function(){
 					refreshRdvData();
@@ -157,9 +188,12 @@ angular.module('myApp.controllers').controller('RdvCtrl',
 			var formatAnnonces = function(aAnnonces){
 				var nbAnnonces = aAnnonces.length;
 
+
 				for(var i=0; i< nbAnnonces;i++){
-					$scope.aAnnoncesFormated[i] = formatAnnonce(aAnnonces[i]);
+					aAnnonces[i] = formatAnnonce(aAnnonces[i]);
 				}
+
+				return aAnnonces;
 			};
 
 
@@ -184,7 +218,13 @@ angular.module('myApp.controllers').controller('RdvCtrl',
 
 					$scope.messageFormAnnonce = 'your annoncement has been send and will appear in few second, please wait';
 
-					//$scope.aRdv.push(formatAnnonce(data));
+					var newAnnonce = [];
+					newAnnonce.push(formatAnnonce(data));
+
+
+					addNewData($scope.aRdv,newAnnonce);
+
+					console.log(newAnnonce,$scope.aRdv);
 
 				}).error(function(data){
 					$scope.annonce_tag = '';
