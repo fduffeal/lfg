69e9beae18dad2bbd095f3518ed477ae38bad3ba
Romain LEBRUN <romain.lebrun@watchever.com>
2015-02-23 15:32:54 +0100

    profile

--- app/dist/all.js
+++ app/dist/all.js
@@ -32247,6 +32247,286 @@ angular.module('myApp.directives')
     ]
 );
 
+angular.module('myApp.filters').filter('filterAvatar', [function () {
+	'use strict';
+	return function (userGameProfil) {
+
+
+		var aFilteredItems = [];
+		var maxLevel = 0;
+
+		for(var key in userGameProfil){
+			var currentLevel = parseInt(userGameProfil[key].data2);
+			if(currentLevel < maxLevel){
+				continue;
+			}
+			maxLevel = currentLevel;
+			aFilteredItems = [];
+			aFilteredItems.push(userGameProfil[key]);
+
+		}
+		return aFilteredItems;
+	};
+}]);
+angular.module('myApp.filters').filter('filterCharacters', function () {
+	'use strict';
+	return function (input, chars, breakOnWord) {
+		if (isNaN(chars)) {
+			return input;
+		}
+		if (chars <= 0) {
+			return '';
+		}
+		if (input && input.length > chars) {
+			input = input.substring(0, chars);
+
+			if (!breakOnWord) {
+				var lastspace = input.lastIndexOf(' ');
+				//get last space
+				if (lastspace !== -1) {
+					input = input.substr(0, lastspace);
+				}
+			} else {
+				while (input.charAt(input.length - 1) === ' ') {
+					input = input.substr(0, input.length - 1);
+				}
+			}
+			return input + '...';
+		}
+		return input;
+	};
+});
+angular.module('myApp.filters').filter('filterDateForum', [
+	'$filter',
+	function ($filter) {
+		'use strict';
+		return function (date2_s) {
+			var date2_ms = date2_s*1000;
+
+			var now = new Date();
+			var date1_ms = now.getTime();
+
+			var dayDisplay = $filter('date')(date2_ms, 'd/MM/yy');
+			if(dayDisplay !== $filter('date')(date1_ms, 'd/MM/yy')){
+				return dayDisplay;
+			}
+			return $filter('date')(date2_ms, 'H:mm');
+		};
+	}
+]);
+angular.module('myApp.filters').filter('filterGameProfil', [function () {
+	'use strict';
+	return function (userGameProfil,gameId,plateformId) {
+		var aFilterdItems = [];
+
+		for(var key in userGameProfil){
+			if(userGameProfil[key].game.id !== gameId && gameId !== null){
+				continue;
+			}
+			if(userGameProfil[key].plateform.id !== plateformId && plateformId !== null){
+				continue;
+			}
+			aFilterdItems.push(userGameProfil[key]);
+		}
+
+		return aFilterdItems;
+	};
+}]);
+angular.module('myApp.filters').filter('filterHtml', ['$sce',
+	function ($sce) {
+	'use strict';
+	return function (input) {
+
+		if (input) {
+			input = $sce.trustAsHtml(input);
+		}
+		return input;
+	};
+}]);
+angular.module('myApp.filters').filter('filterOnlineTime', [
+	'$filter','gettextCatalog',
+	function ($filter,gettextCatalog) {
+		'use strict';
+		return function (date2_s) {
+			var date2_ms = date2_s*1000;
+			var currentDate = new Date(date2_ms);
+
+			var now = new Date();
+			var date1_ms = now.getTime();
+			// Calculate the difference in milliseconds
+			var difference_ms = date1_ms - date2_ms;
+
+			var tomorrow = new Date(date1_ms + 24 * 60 * 60 * 1000);
+
+			var one_minute=1000*60*1;
+
+			var diffMinutes = Math.round(difference_ms/one_minute);
+			if(diffMinutes < 60){
+				return gettextCatalog.getString("{{minutes}} minutes", { minutes: diffMinutes });
+			}
+
+			var one_hour=1000*60*60*1;
+			var diffHours = Math.round(difference_ms/one_hour);
+			if(diffHours < 24){
+				return gettextCatalog.getString("{{hours}} hours", { hours: diffHours });
+
+			}
+
+			//Get 1 day in milliseconds
+			var one_day=1000*60*60*24;
+			var diffDays = Math.round(difference_ms/one_day);
+			return gettextCatalog.getString("{{days}} days", { days: diffDays });
+		};
+	}
+]);
+angular.module('myApp.filters').filter('filterRdv', [function () {
+	'use strict';
+	return function (items,plateformId,tags) {
+
+		var aFilterdItems = [];
+
+		var aTags = [];
+		if(typeof tags === "string" && tags !== ""){
+			aTags = tags.split(' ');
+		}
+
+		var d = new Date();
+		var now = d.getTime()/1000;
+
+		for(var key in items){
+			if(plateformId !== ""){
+				if(items[key].plateform === null || items[key].plateform.id !== plateformId){
+					continue;
+				}
+			}
+
+			if(items[key].end < now){
+				continue;
+			}
+
+			if(aTags.length === 0){
+				aFilterdItems.push(items[key]);
+				continue;
+			}
+
+			var aTagItem = [];
+			for(var keyTagItem in items[key].tags){
+				aTagItem.push(items[key].tags[keyTagItem].nom.toLowerCase());
+			}
+
+			var asTag = true;
+			for(var keyTag in aTags){
+				if(aTagItem.indexOf(aTags[keyTag].toLowerCase()) < 0){
+					asTag = false;
+				}
+			}
+
+			if(asTag === false){
+				continue;
+			}
+
+			aFilterdItems.push(items[key]);
+		}
+
+		return aFilterdItems;
+	};
+}]);
+angular.module('myApp.filters').filter('filterRdvLastPlace', [
+	'filter',
+	function (filter) {
+	'use strict';
+	return function (items,plateformId,tags,onlyLive,onlyInFuture,onlyWithPlace,onlyOnePlace,nbPlaceAvailable,type) {
+
+		return filter.byPlateformsAndTags(items,plateformId,tags,onlyLive,onlyInFuture,onlyWithPlace,onlyOnePlace,nbPlaceAvailable,type);
+
+	};
+}]);
+angular.module('myApp.filters').filter('filterRdvWithMe', [
+	'filter',
+	function (filter) {
+	'use strict';
+	return function (items,currentUserId,plateformId,tags,onlyOnePlace,nbPlaceAvailable) {
+
+		return filter.byPlateformsAndTagsWithMe(items,currentUserId,plateformId,tags,onlyOnePlace,nbPlaceAvailable);
+
+	};
+}]);
+angular.module('myApp.filters').filter('filterSince', ['tools',
+	function (tools) {
+		'use strict';
+		return function (time_s) {
+			return tools.getSinceTimeMessage(time_s);
+		};
+	}
+]);
+angular.module('myApp.filters').filter('filterUser', [
+	function () {
+		'use strict';
+		return function (userList,usernameSearch,onlyFriends,plateformId) {
+
+			if(usernameSearch == '' && plateformId === null){
+				return userList;
+			}
+
+			usernameSearch = usernameSearch.toLowerCase();
+
+			var userFiltered = [];
+			for(var key in userList){
+
+				var isGoodPlateform = true;
+
+				if(plateformId !== null && typeof plateformId !== "undefined"){
+					isGoodPlateform = false;
+					for(var i in userList[key].userGame){
+						if(userList[key].userGame[i].plateform.id === plateformId){
+							isGoodPlateform = true;
+							break;
+						}
+					}
+				}
+
+				if(isGoodPlateform === false){
+					continue;
+				}
+
+
+				var findUser = false;
+				for(var i in userList[key].userGame){
+					if(userList[key].userGame[i].gameUsername.toLowerCase().indexOf(usernameSearch) != -1){
+						findUser = true;
+						break;
+					}
+				}
+
+				if(findUser === false){
+					continue;
+				}
+
+				userFiltered.push(userList[key]);
+
+			}
+			return userFiltered;
+		};
+	}
+]);
+angular.module('myApp.filters').filter('filterWords', function () {
+	'use strict';
+	return function (input, words) {
+		if (isNaN(words)) {
+			return input;
+		}
+		if (words <= 0) {
+			return '';
+		}
+		if (input) {
+			var inputWords = input.split(/\s+/);
+			if (inputWords.length > words) {
+				input = inputWords.slice(0, words).join(' ') + '...';
+			}
+		}
+		return input;
+	};
+});
 angular.module('superCache',[])
 	.factory('superCache', ['$cacheFactory','$q','$timeout',
 		function($cacheFactory,$q,$timeout) {
@@ -33567,284 +33847,3 @@ angular.module('myApp.services')
 		}
 	]
 );
-
-angular.module('myApp.filters').filter('filterAvatar', [function () {
-	'use strict';
-	return function (userGameProfil) {
-
-
-		var aFilteredItems = [];
-		var maxLevel = 0;
-
-		for(var key in userGameProfil){
-			var currentLevel = parseInt(userGameProfil[key].data2);
-			if(currentLevel < maxLevel){
-				continue;
-			}
-			maxLevel = currentLevel;
-			aFilteredItems = [];
-			aFilteredItems.push(userGameProfil[key]);
-
-		}
-		return aFilteredItems;
-	};
-}]);
-angular.module('myApp.filters').filter('filterCharacters', function () {
-	'use strict';
-	return function (input, chars, breakOnWord) {
-		if (isNaN(chars)) {
-			return input;
-		}
-		if (chars <= 0) {
-			return '';
-		}
-		if (input && input.length > chars) {
-			input = input.substring(0, chars);
-
-			if (!breakOnWord) {
-				var lastspace = input.lastIndexOf(' ');
-				//get last space
-				if (lastspace !== -1) {
-					input = input.substr(0, lastspace);
-				}
-			} else {
-				while (input.charAt(input.length - 1) === ' ') {
-					input = input.substr(0, input.length - 1);
-				}
-			}
-			return input + '...';
-		}
-		return input;
-	};
-});
-angular.module('myApp.filters').filter('filterDateForum', [
-	'$filter',
-	function ($filter) {
-		'use strict';
-		return function (date2_s) {
-			var date2_ms = date2_s*1000;
-
-			var now = new Date();
-			var date1_ms = now.getTime();
-
-			var dayDisplay = $filter('date')(date2_ms, 'd/MM/yy');
-			if(dayDisplay !== $filter('date')(date1_ms, 'd/MM/yy')){
-				return dayDisplay;
-			}
-			return $filter('date')(date2_ms, 'H:mm');
-		};
-	}
-]);
-angular.module('myApp.filters').filter('filterGameProfil', [function () {
-	'use strict';
-	return function (userGameProfil,gameId,plateformId) {
-		var aFilterdItems = [];
-
-		for(var key in userGameProfil){
-			if(userGameProfil[key].game.id !== gameId && gameId !== null){
-				continue;
-			}
-			if(userGameProfil[key].plateform.id !== plateformId && plateformId !== null){
-				continue;
-			}
-			aFilterdItems.push(userGameProfil[key]);
-		}
-
-		return aFilterdItems;
-	};
-}]);
-angular.module('myApp.filters').filter('filterHtml', ['$sce',
-	function ($sce) {
-	'use strict';
-	return function (input) {
-
-		if (input) {
-			input = $sce.trustAsHtml(input);
-		}
-		return input;
-	};
-}]);
-angular.module('myApp.filters').filter('filterOnlineTime', [
-	'$filter','gettextCatalog',
-	function ($filter,gettextCatalog) {
-		'use strict';
-		return function (date2_s) {
-			var date2_ms = date2_s*1000;
-			var currentDate = new Date(date2_ms);
-
-			var now = new Date();
-			var date1_ms = now.getTime();
-			// Calculate the difference in milliseconds
-			var difference_ms = date1_ms - date2_ms;
-
-			var tomorrow = new Date(date1_ms + 24 * 60 * 60 * 1000);
-
-			var one_minute=1000*60*1;
-
-			var diffMinutes = Math.round(difference_ms/one_minute);
-			if(diffMinutes < 60){
-				return gettextCatalog.getString("{{minutes}} minutes", { minutes: diffMinutes });
-			}
-
-			var one_hour=1000*60*60*1;
-			var diffHours = Math.round(difference_ms/one_hour);
-			if(diffHours < 24){
-				return gettextCatalog.getString("{{hours}} hours", { hours: diffHours });
-
-			}
-
-			//Get 1 day in milliseconds
-			var one_day=1000*60*60*24;
-			var diffDays = Math.round(difference_ms/one_day);
-			return gettextCatalog.getString("{{days}} days", { days: diffDays });
-		};
-	}
-]);
-angular.module('myApp.filters').filter('filterRdv', [function () {
-	'use strict';
-	return function (items,plateformId,tags) {
-
-		var aFilterdItems = [];
-
-		var aTags = [];
-		if(typeof tags === "string" && tags !== ""){
-			aTags = tags.split(' ');
-		}
-
-		var d = new Date();
-		var now = d.getTime()/1000;
-
-		for(var key in items){
-			if(plateformId !== ""){
-				if(items[key].plateform === null || items[key].plateform.id !== plateformId){
-					continue;
-				}
-			}
-
-			if(items[key].end < now){
-				continue;
-			}
-
-			if(aTags.length === 0){
-				aFilterdItems.push(items[key]);
-				continue;
-			}
-
-			var aTagItem = [];
-			for(var keyTagItem in items[key].tags){
-				aTagItem.push(items[key].tags[keyTagItem].nom.toLowerCase());
-			}
-
-			var asTag = true;
-			for(var keyTag in aTags){
-				if(aTagItem.indexOf(aTags[keyTag].toLowerCase()) < 0){
-					asTag = false;
-				}
-			}
-
-			if(asTag === false){
-				continue;
-			}
-
-			aFilterdItems.push(items[key]);
-		}
-
-		return aFilterdItems;
-	};
-}]);
-angular.module('myApp.filters').filter('filterRdvLastPlace', [
-	'filter',
-	function (filter) {
-	'use strict';
-	return function (items,plateformId,tags,onlyLive,onlyInFuture,onlyWithPlace,onlyOnePlace,nbPlaceAvailable,type) {
-
-		return filter.byPlateformsAndTags(items,plateformId,tags,onlyLive,onlyInFuture,onlyWithPlace,onlyOnePlace,nbPlaceAvailable,type);
-
-	};
-}]);
-angular.module('myApp.filters').filter('filterRdvWithMe', [
-	'filter',
-	function (filter) {
-	'use strict';
-	return function (items,currentUserId,plateformId,tags,onlyOnePlace,nbPlaceAvailable) {
-
-		return filter.byPlateformsAndTagsWithMe(items,currentUserId,plateformId,tags,onlyOnePlace,nbPlaceAvailable);
-
-	};
-}]);
-angular.module('myApp.filters').filter('filterSince', ['tools',
-	function (tools) {
-		'use strict';
-		return function (time_s) {
-			return tools.getSinceTimeMessage(time_s);
-		};
-	}
-]);
-angular.module('myApp.filters').filter('filterUser', [
-	function () {
-		'use strict';
-		return function (userList,usernameSearch,onlyFriends,plateformId) {
-
-			if(usernameSearch == '' && plateformId === null){
-				return userList;
-			}
-
-			usernameSearch = usernameSearch.toLowerCase();
-
-			var userFiltered = [];
-			for(var key in userList){
-
-				var isGoodPlateform = true;
-
-				if(plateformId !== null && typeof plateformId !== "undefined"){
-					isGoodPlateform = false;
-					for(var i in userList[key].userGame){
-						if(userList[key].userGame[i].plateform.id === plateformId){
-							isGoodPlateform = true;
-							break;
-						}
-					}
-				}
-
-				if(isGoodPlateform === false){
-					continue;
-				}
-
-
-				var findUser = false;
-				for(var i in userList[key].userGame){
-					if(userList[key].userGame[i].gameUsername.toLowerCase().indexOf(usernameSearch) != -1){
-						findUser = true;
-						break;
-					}
-				}
-
-				if(findUser === false){
-					continue;
-				}
-
-				userFiltered.push(userList[key]);
-
-			}
-			return userFiltered;
-		};
-	}
-]);
-angular.module('myApp.filters').filter('filterWords', function () {
-	'use strict';
-	return function (input, words) {
-		if (isNaN(words)) {
-			return input;
-		}
-		if (words <= 0) {
-			return '';
-		}
-		if (input) {
-			var inputWords = input.split(/\s+/);
-			if (inputWords.length > words) {
-				input = inputWords.slice(0, words).join(' ') + '...';
-			}
-		}
-		return input;
-	};
-});
