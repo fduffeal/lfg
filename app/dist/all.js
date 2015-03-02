b011b7ccef0c68c9569d2b6a220c702363da3dc3
Fumistar <francis.duffeal@gmail.com>
2015-03-02 22:54:04 +0100

    videotheque

--- app/dist/all.js
+++ app/dist/all.js
@@ -30071,8 +30071,8 @@ angular.module('myApp.controllers').controller('GamesProfilesCtrl',
 );
 
 angular.module('myApp.controllers').controller('HomeCtrl',
-	['$scope','$routeParams','forum','redirection','$anchorScroll','$location','$timeout','user','$window',
-	function ($scope,$routeParams,forum,redirection,$anchorScroll,$location,$timeout,user,$window) {
+	['$scope','$routeParams','forum','redirection','$anchorScroll','$location','$timeout','user','rdv',
+	function ($scope,$routeParams,forum,redirection,$anchorScroll,$location,$timeout,user,rdv) {
 			'use strict';
 			/*$scope.msg = $routeParams.msg;
 			console.log($scope.msg);*/
@@ -30089,6 +30089,34 @@ angular.module('myApp.controllers').controller('HomeCtrl',
 					});
 				}*/
 			});
+
+
+			var formatRdv = function(rdv){
+				for(var key in rdv.users){
+					if(rdv.users[key].user.id === rdv.leader.id){
+						rdv.author = rdv.users[key];
+						rdv.user = rdv.users[key].user;
+						rdv.type = 'type_party';
+					}
+				}
+			};
+			var refreshRdvData = function(){
+
+				rdv.getAll().success(function(data) {
+					// this callback will be called asynchronously
+					// when the response is available
+					for(var key in data){
+						data[key].url = $scope.partyWaitingUrlRoot+data[key].id;
+						formatRdv(data[key]);
+					}
+					$scope.aRdv = data;
+
+				}).error(function(data, status, headers, config) {
+					// called asynchronously if an error occurs
+					// or server returns response with an error status.
+				});
+
+			};
 
 		}
 	]
