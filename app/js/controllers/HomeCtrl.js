905da71dcf37d17125e4d15333a580a521d8e79a
Fumistar <francis.duffeal@gmail.com>
2015-03-02 22:30:29 +0100

    player youtube

--- app/js/controllers/HomeCtrl.js
+++ app/js/controllers/HomeCtrl.js
@@ -1,7 +1,7 @@
 angular.module('myApp.controllers').controller('HomeCtrl',
 	['$scope','$routeParams','forum','redirection','$anchorScroll','$location','$timeout','user','$window',
 	function ($scope,$routeParams,forum,redirection,$anchorScroll,$location,$timeout,user,$window) {
-		'use strict';
+			'use strict';
 			/*$scope.msg = $routeParams.msg;
 			console.log($scope.msg);*/
 			$scope.currentUser = user.get();
@@ -11,14 +11,13 @@ angular.module('myApp.controllers').controller('HomeCtrl',
 			forum.getAllTopic().success(function(data) {
 				$scope.aTopic = data;
 
-				for(var i = 0;i< $scope.aTopic.length-1;i++){
+				/*for(var i = 0;i< $scope.aTopic.length-1;i++){
 					forum.getTopic($scope.aTopic[i].id, 1, 1).success(function(data) {
 						$scope.aTopic[i].message = data.messages[0].texte;
 					});
-				}
+				}*/
 			});
 
-
 		}
-		]
-		);
+	]
+);
